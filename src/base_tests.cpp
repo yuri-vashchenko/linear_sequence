54847e9dd7186682db8f8d101627f8b6155cc93f
Yuri Vashchenko <yuri.vashchenko@gmail.com>
2019-07-08 16:36:45 +1000

    add more tests

--- src/base_tests.cpp
+++ src/base_tests.cpp
@@ -7,10 +7,58 @@ extern "C"
 
 TEST_GROUP(BasicTests)
 {
+    LSQ_HangleT handle = NULL;
+    void setup()
+    {
+        handle = LSQ_CreateSequence();
+    }
+
+    void teardoen()
+    {
+        LSQ_DestroySequence(handle);
+    }
 };
 
 TEST(BasicTests, CreateContainer)
 {
-    LSQ_HandleT handle = LSQ_CreateSequence();
     CHECK_TEXT(handle != NULL, "LSQ_CreateSequence returned NULL");
 }
+
+TEST(BasicTests, DestroyContainer)
+{
+}
+
+TEST(BasicTests, AddFrontElement)
+{
+    CHECK_EQUAL(0, LSQ_GetSize(handle));
+    LSQ_BaseTypeT element = 123;
+    LSQ_InsertFrontElement(handle, element);
+    // Check number of Elements
+    CHECK_EQUAL(1, LSQ_GetSize(handle));
+    // Check front Element
+    // Create iterator
+    LSQ_IteratorT iterator = LSQ_GetFrontElement(handle);
+    CHECK_TEXT(iterator != NULL, "LSQ_GetFrontElement returned NULL");
+    // Check it is dereferencable
+    CHECK_TEXT(LSQ_IsIteratorDereferencable(iterator), "Created iterator is not dereferencable");
+    // Check value
+    CHECK_EQUAL(element, *LSQ_DereferenceIterator(iterator));
+}
+
+TEST(BasicTests, AddRearElement)
+{
+    CHECK_EQUAL(0, LSQ_GetSize(handle));
+    LSQ_BaseTypeT element = 321;
+    LSQ_InsertRearElement(handle, element);
+    // Check number of Elements
+    CHECK_EQUAL(1, LSQ_GetSize(handle));
+    // Check front Element
+    // Create iterator
+    LSQ_IteratorT iterator = LSQ_GetFrontElement(handle);
+    CHECK_TEXT(iterator != NULL, "LSQ_GetFrontElement returned NULL");
+    // Check it is dereferencable
+    CHECK_TEXT(LSQ_IsIteratorDereferencable(iterator), "Created iterator is not dereferencable");
+    // Check value
+    CHECK_EQUAL(element, *LSQ_DereferenceIterator(iterator));
+}
+
